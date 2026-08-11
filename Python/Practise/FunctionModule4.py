ba858185db1250064aed7dd91beae7a2782a3235
Nikhil Aghav <nikhilaaghav@gmail.com>
2026-08-11 23:37:44 +0530

    Add files via upload

---
+++ Python/Practise/FunctionModule4.py
@@ -0,0 +1,21 @@
+from Marvellous import Addition,Subtraction
+
+
+def main():
+    print("Enter first number:")
+    value1 = int(input())
+
+    print("Enter second number:")
+    value2 = int(input())
+
+    Ret = Addition(value1 , value2)  
+
+    print("Addition is:",Ret)
+
+    Ret = Subtraction(value1, value2)  
+
+    print("Subtraction is:",Ret)
+
+if __name__ =="__main__":
+    main()
+
---
+++ Python/Practise/Marvellous.py
@@ -0,0 +1,8 @@
+def Addition(No1,No2):
+    Ans = 0
+    Ans = No1 + No2
+    return Ans
+
+def Subtraction(No1 , No2):
+    Ans = No1 - No2
+    return Ans
